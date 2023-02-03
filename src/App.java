a38d2d5a9613acd2f7b6169f40c59fe9ff9b4d78
akouvivien <akouvivien@gmail.com>
2023-02-03 17:46:46 +0000

    correction DailyChallenge

--- src/App.java
+++ src/App.java
@@ -3,27 +3,46 @@ import java.util.Scanner;
 
 public class App {
 
-    public static  void  choicePlay( int nombre, int n) {
+    public static  void  choicePlay(  int n) {
+        Scanner scanner = new Scanner(System.in);
+        int b =3;
+            System.out.println("vous avez "+b+" chances");
+            
+        //execution de la comparaison  
+        
+            int i = 1;
+            do {
+                System.out.println("------------------operation "+ i +" -------------------");
+                System.out.print("devinez un nombre ");
+                 int nombre = scanner.nextInt();
 
-        System.out.println("devinez un nombre");
-        System.out.println("Votre nombre est " + nombre);
-        if (nombre == n) {
-            System.out.println("vous avez gagnez");
-        } else  System.out.println("vous avez perdu");
+                 b--;
+                 System.out.println("nombre de chances restant "+b);
+                if (nombre < n) {
+                    System.out.println("le Nombre est plus petit");
+                    
+                } 
+                else if (nombre > n) {
+                    System.out.println("le Nombre est plus grand");
+                } 
+                else  {
+                    System.out.println("vous avez gagnez");
+                    break;}
+
+                i++;
+            } while (i <= 3);
     }
+
+
 
     public static void main(String[] args) throws Exception {
         //nombre aleatoire
         Random rand = new Random();
         int n = rand.nextInt(100);
         System.out.println("le nombre choix par l'ordianteur est " + n);
+
         //choix du nombre par l'utilisateur
-        Scanner scanner = new Scanner(System.in);
-        int nombre = scanner.nextInt();  
-        //execution de la comparaison   
-        choicePlay(nombre, n);
-
+           choicePlay(n);
+  
     }
-
-    
 }
